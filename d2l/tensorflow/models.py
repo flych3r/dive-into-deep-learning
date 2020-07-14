0f5312a73f16ca574ca7e9fa55591df2ee77af39
flych3r <xavier_sampaio@alu.ufc.br>
2020-07-13 21:53:22 -0300

    rnn

--- d2l/tensorflow/models.py
+++ d2l/tensorflow/models.py
@@ -4,6 +4,10 @@ from d2l.tensorflow.activations import relu
 from d2l.tensorflow.metrics import Accumulator
 from d2l.tensorflow.plot import Animator
 from d2l.tensorflow.regularization import dropout
+from d2l.tensorflow.data import seq_data_iter_consecutive, seq_data_iter_random
+import time
+import math
+from tqdm import tqdm
 
 
 def linreg(X, W, b):
@@ -31,6 +35,104 @@ def mlp(X, W, b, drop_rates, num_inputs, inference=False):
 
     return tf.matmul(X, W[i + 1]) + b[i + 1]
 
+
+def to_onehot(X, size):
+    ones = [tf.one_hot(x, size) for x in tf.transpose(X)]
+    return tf.stack(ones)
+
+
+def predict_rnn(
+    prefix, num_chars, rnn, params, init_rnn_state,
+    num_hiddens, vocab_size, idx_to_char, char_to_idx
+):
+    state = init_rnn_state(1, num_hiddens)
+    output = [char_to_idx[prefix[0]]]
+    for t in range(num_chars + len(prefix) - 1):
+        # The output of the previous time step is taken
+        # as the input of the current time step.
+        X = to_onehot([output[-1]], vocab_size)
+        # Calculate the output and update the hidden state.
+        (Y, state) = rnn([X], state, params)
+        # The input to the next time step is the character in 3
+        # the prefix or the current best predicted character.
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t + 1]])
+        else:
+            # This is maximum likelihood decoding, not sampling
+            output.append(int(tf.argmax(Y[0], axis=1)))
+    return ''.join([idx_to_char[i] for i in output])
+
+
+def grad_clipping(gradients, theta):
+    gradients = [tf.Variable(grad) for grad in gradients]
+    norm = tf.Variable(0.)
+    for grad in gradients:
+        norm.assign_add(tf.reduce_sum(grad ** 2))
+        norm.assign(tf.sqrt(norm))
+    if norm > theta:
+        for grad in gradients:
+            grad.assign_add(grad * (theta / norm))
+    return gradients
+
+def train_and_predict_rnn(rnn, loss, optimizer, get_params, init_rnn_state, num_hiddens,
+                          vocab_size, corpus_indices, idx_to_char,
+                          char_to_idx, is_random_iter, num_epochs, num_steps,
+                          lr, clipping_theta, batch_size, pred_period,
+                          pred_len, prefixes):
+    if is_random_iter:
+        data_iter_fn = seq_data_iter_random
+    else:
+        data_iter_fn = seq_data_iter_consecutive
+    params = get_params()
+    
+    for epoch in tqdm(range(num_epochs)):
+        if not is_random_iter:  
+            # If adjacent sampling is used, the hidden state is initialized 
+            # at the beginning of the epoch.
+            state = init_rnn_state(batch_size, num_hiddens)
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps)
+        for X, Y in data_iter:
+            if is_random_iter:  
+                # If random sampling is used, the hidden state is initialized 
+                # before each mini-batch update.
+                state = init_rnn_state(batch_size, num_hiddens)
+            # else:  
+            #     # Otherwise, the detach function needs to be used to separate 
+            #     # the hidden state from the computational graph to avoid 
+            #     # backpropagation beyond the current sample.
+            #     for s in state:
+            #         s.detach()
+            with tf.GradientTape() as t:
+                inputs = to_onehot(X, vocab_size)
+                # outputs is num_steps terms of shape (batch_size, vocab_size)
+                (outputs, state) = rnn(inputs, state, params)
+                # after stitching it is (num_steps * batch_size, vocab_size).
+                outputs = tf.concat(outputs, axis=0)
+                # The shape of Y is (batch_size, num_steps), and then becomes 
+                # a vector with a length of batch * num_steps after 
+                # transposition. This gives it a one-to-one correspondence 
+                # with output rows.
+                y = tf.reshape(tf.transpose(Y), (-1,))
+                # Average classification error via cross entropy loss.
+                l = tf.reduce_mean(loss(y, outputs))
+            gradients = t.gradient(l, params)
+            gradients = grad_clipping(gradients, clipping_theta)  # Clip the gradient.
+            optimizer(params, gradients, lr, 1)  
+            # Since the error is the mean, no need to average gradients here.
+            l_sum += l * y.shape[0]
+            n += y.shape[0]
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            for prefix in prefixes:
+                print(' -', 
+                    predict_rnn(
+                        prefix, pred_len, rnn, params, init_rnn_state,
+                        num_hiddens, vocab_size, idx_to_char, char_to_idx
+                    )
+                )
 
 class BaseModel:
     _identifier = -1
@@ -138,3 +240,6 @@ class Sequential(BaseModel):
             'name': self.__name__,
             'layers': self.layers,
         })
+
+
+
